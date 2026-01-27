04288331d62a6a87d610e983607e67a8a3eec2d7
mu-stf <harvykj30@gmail.com>
2026-01-27 13:34:43 +0300

    تحديث إعدادات Next.js وتحسين تتبع الملفات

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-      root: __dirname,
-    },
-  },
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
